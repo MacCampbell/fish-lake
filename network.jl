4eb5725949f6a92de007ad80fdfc2b7d858f099d
Mac Campbell <mac@macs-mbp.building.sydney.edu.au>
2024-05-02 11:30:16 +1000

    set up hmax=5 for tui chub

--- network.jl
+++ network.jl
@@ -11,21 +11,23 @@ tree = readTopology(treefile);
 T=readTopologyLevel1(treefile);    
 
 # Using snaq!
-net0 = snaq!(T,CF, hmax=0, filename="/home/maccamp/gadopsis/outputs/network/net0", seed=1234);      
-writeTopology(net0, "/home/maccamp/gadopsis/outputs/network/bestnet-h0.tre")
+net0 = snaq!(T,CF, hmax=0, filename="/home/maccamp/network/net0", seed=1234);      
+writeTopology(net0, "/home/maccamp/network/bestnet-h0.tre")
 
-net1 = snaq!(T,CF, hmax=1, filename="/home/maccamp/gadopsis/outputs/network/net1", seed=1234);  
-writeTopology(net1, "/home/maccamp/gadopsis/outputs/network/bestnet-h1.tre")
+net1 = snaq!(T,CF, hmax=1, filename="/home/maccamp/network/net1", seed=1234);  
+writeTopology(net1, "/home/maccamp/network/bestnet-h1.tre")
 
-net2 = snaq!(T,CF, hmax=2, filename="/home/maccamp/gadopsis/outputs/network/net2", seed=1234);      
-writeTopology(net2, "/home/maccamp/gadopsis/outputs/network/bestnet-h2.tre")
+net2 = snaq!(T,CF, hmax=2, filename="/home/maccamp/network/net2", seed=1234);      
+writeTopology(net2, "/home/maccamp/network/bestnet-h2.tre")
 
-net3 = snaq!(T,CF, hmax=3, filename="/home/maccamp/gadopsis/outputs/network/net3", seed=1234);      
-writeTopology(net3, "/home/maccamp/gadopsis/outputs/network/bestnet-h3.tre")
+net3 = snaq!(T,CF, hmax=3, filename="/home/maccamp/network/net3", seed=1234);      
+writeTopology(net3, "/home/maccamp/network/bestnet-h3.tre")
 
-net4 = snaq!(T,CF, hmax=4, filename="/home/maccamp/gadopsis/outputs/network/net4", seed=1234);      
-writeTopology(net4, "/home/maccamp/gadopsis/outputs/network/bestnet-h4.tre")
+net4 = snaq!(T,CF, hmax=4, filename="/home/maccamp/network/net4", seed=1234);      
+writeTopology(net4, "/home/maccamp/network/bestnet-h4.tre")
 
+net5 = snaq!(T,CF, hmax=5, filename="/home/maccamp/network/net5", seed=1234);      
+writeTopology(net5, "/home/maccamp/network/bestnet-h5.tre")
 
 using RCall      
 imagefilename = "outputs/network/snaqplot-net0.pdf"
@@ -48,8 +50,14 @@ R"par"(mar=[0,0,0,0]) # to reduce margins
 plot(net2, showgamma=true, showedgenumber=true);
 R"dev.off()"; 
 
+using RCall      
+imagefilename = "outputs/network/snaqplot-net3.pdf"
+R"pdf"(imagefilename, width=4, height=3) 
+R"par"(mar=[0,0,0,0]) # to reduce margins 
+plot(net3, showgamma=true, showedgenumber=true);
+R"dev.off()"; 
 
-scores = [net0.loglik, net1.loglik, net2.loglik, net3.loglik, net4.loglik]
+scores = [net0.loglik, net1.loglik, net2.loglik, net3.loglik, net4.loglik, net5.loglik]
 hmax = collect(0:4)
 
 using RCall      
