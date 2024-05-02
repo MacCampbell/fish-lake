2aeedc8576b1b507cf47f42c52d11095399041ea
Mac Campbell <mac@macs-mbp.building.sydney.edu.au>
2024-05-02 11:33:37 +1000

    Setting output to outputs/107/network

--- network.jl
+++ network.jl
@@ -11,47 +11,47 @@ tree = readTopology(treefile);
 T=readTopologyLevel1(treefile);    
 
 # Using snaq!
-net0 = snaq!(T,CF, hmax=0, filename="/home/maccamp/network/net0", seed=1234);      
-writeTopology(net0, "/home/maccamp/network/bestnet-h0.tre")
+net0 = snaq!(T,CF, hmax=0, filename="/home/maccamp/fish-lake/outputs/107/network/net0", seed=1234);      
+writeTopology(net0, "/home/maccamp/fish-lake/outputs/107/network/bestnet-h0.tre")
 
-net1 = snaq!(T,CF, hmax=1, filename="/home/maccamp/network/net1", seed=1234);  
-writeTopology(net1, "/home/maccamp/network/bestnet-h1.tre")
+net1 = snaq!(T,CF, hmax=1, filename="/home/maccamp/fish-lake/outputs/107/network/net1", seed=1234);  
+writeTopology(net1, "/home/maccamp/fish-lake/outputs/107/network/bestnet-h1.tre")
 
-net2 = snaq!(T,CF, hmax=2, filename="/home/maccamp/network/net2", seed=1234);      
-writeTopology(net2, "/home/maccamp/network/bestnet-h2.tre")
+net2 = snaq!(T,CF, hmax=2, filename="/home/maccamp/fish-lake/outputs/107/network/net2", seed=1234);      
+writeTopology(net2, "/home/maccamp/fish-lake/outputs/107/network/bestnet-h2.tre")
 
-net3 = snaq!(T,CF, hmax=3, filename="/home/maccamp/network/net3", seed=1234);      
-writeTopology(net3, "/home/maccamp/network/bestnet-h3.tre")
+net3 = snaq!(T,CF, hmax=3, filename="/home/maccamp/fish-lake/outputs/107/network/net3", seed=1234);      
+writeTopology(net3, "/home/maccamp/fish-lake/outputs/107/network/bestnet-h3.tre")
 
-net4 = snaq!(T,CF, hmax=4, filename="/home/maccamp/network/net4", seed=1234);      
-writeTopology(net4, "/home/maccamp/network/bestnet-h4.tre")
+net4 = snaq!(T,CF, hmax=4, filename="/home/maccamp/fish-lake/outputs/107/network/net4", seed=1234);      
+writeTopology(net4, "/home/maccamp/fish-lake/outputs/107/network/bestnet-h4.tre")
 
-net5 = snaq!(T,CF, hmax=5, filename="/home/maccamp/network/net5", seed=1234);      
-writeTopology(net5, "/home/maccamp/network/bestnet-h5.tre")
+net5 = snaq!(T,CF, hmax=5, filename="/home/maccamp/fish-lake/outputs/107/network/net5", seed=1234);      
+writeTopology(net5, "/home/maccamp/fish-lake/outputs/107/network/bestnet-h5.tre")
 
 using RCall      
-imagefilename = "outputs/network/snaqplot-net0.pdf"
+imagefilename = "outputs/fish-lake/outputs/107/network/snaqplot-net0.pdf"
 R"pdf"(imagefilename, width=4, height=3) 
 R"par"(mar=[0,0,0,0]) 
 plot(net0, showgamma=true, showedgenumber=true);
 R"dev.off()"; 
 
 using RCall     
-imagefilename = "outputs/network/snaqplot-net1.pdf"
+imagefilename = "outputs/fish-lake/outputs/107/network/snaqplot-net1.pdf"
 R"pdf"(imagefilename, width=4, height=3) 
 R"par"(mar=[0,0,0,0]) 
 plot(net1, showgamma=true, showedgenumber=true); 
 R"dev.off()"; 
 
 using RCall      
-imagefilename = "outputs/network/snaqplot-net2.pdf"
+imagefilename = "outputs/fish-lake/outputs/107/network/snaqplot-net2.pdf"
 R"pdf"(imagefilename, width=4, height=3) 
 R"par"(mar=[0,0,0,0]) # to reduce margins 
 plot(net2, showgamma=true, showedgenumber=true);
 R"dev.off()"; 
 
 using RCall      
-imagefilename = "outputs/network/snaqplot-net3.pdf"
+imagefilename = "outputs/fish-lake/outputs/107/network/snaqplot-net3.pdf"
 R"pdf"(imagefilename, width=4, height=3) 
 R"par"(mar=[0,0,0,0]) # to reduce margins 
 plot(net3, showgamma=true, showedgenumber=true);
@@ -61,7 +61,7 @@ scores = [net0.loglik, net1.loglik, net2.loglik, net3.loglik, net4.loglik, net5.
 hmax = collect(0:4)
 
 using RCall      
-imagefilename1 = "outputs/network/hscores.pdf"
+imagefilename1 = "outputs/fish-lake/outputs/107/network/hscores.pdf"
 R"pdf"(imagefilename1, width=4, height=3) 
 R"par"(mar=[0,0,0,0]) 
 R"plot"(hmax, scores, type="b", ylab="network score", xlab="hmax", col="blue");
